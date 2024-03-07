79ee85c0c1e0f102596e5353f63f935dc896a4cf
Sreenvitha Chikati <srechi453.fusdk12@gmail.com>
2024-03-06 21:35:07 -0800

    updates so far

--- script/spotify.js
+++ script/spotify.js
@@ -2,7 +2,7 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
     const element = document.getElementById('embed-iframe');
     const options = {
       width: '100%',
-      height: '600px',
+      height: '500px',
       uri: 'spotify:playlist:7JYvz7BubssMOUFsfph1tC'
     };
     const callback = (EmbedController) => {
